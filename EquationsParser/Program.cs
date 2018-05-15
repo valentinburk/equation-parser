2244fa7b9196bfe8b8321905ae8b30b13b01cd87
Valentin Burkin <burkinman@me.com>
2018-05-15 11:47:14 +0300

    Add project files.

---
+++ EquationsParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EquationsParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
